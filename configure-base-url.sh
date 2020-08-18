ed90a824a9461382da5d87cf43fd99b5d32d4fd8
IncPlusPlus <cloherty.ryan@gmail.com>
2020-08-18 13:42:07 -0400

    Update teamware version numbers in example config and shell script
    Also added a specific version number for TeamCity in configure-base-url.sh as using `latest` is usually discouraged.

--- configure-base-url.sh
+++ configure-base-url.sh
@@ -2,20 +2,20 @@
 
 docker run --rm -it \
 -v /opt/jetbrains/youtrack/conf:/opt/youtrack/conf \
-jetbrains/youtrack:2020.1.3588 \
+jetbrains/youtrack:2020.3.4313 \
 configure --base-url=https://youtrack.incplusplus.dev:443
 
 docker run --rm -it \
 -v /opt/jetbrains/hub/conf:/opt/hub/conf \
-jetbrains/hub:2020.1.12157 \
+jetbrains/hub:2020.1.12375 \
 configure --base-url=https://hub.incplusplus.dev:443
 
 docker run --rm -it \
 -v /opt/jetbrains/upsource/conf:/opt/upsource/conf \
-jetbrains/upsource:2019.1.1644 \
+jetbrains/upsource:2020.1.1802 \
 configure --base-url=https://upsource.incplusplus.dev:443
 
 docker run --rm -it \
 -v /opt/jetbrains/teamcity/data:/data/teamcity_server/datadir \
-jetbrains/teamcity-server \
+jetbrains/teamcity-server:2020.1.3 \
 configure --base-url=https://teamcity.incplusplus.dev:443
